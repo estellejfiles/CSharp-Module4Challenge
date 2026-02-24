47165e410ae1f1f868f5eb1f558315454486c755
Estelle Peterson <estellejpeterson101@gmail.com>
2026-02-24 08:32:41 +0000

    Added html for first set of jokes to display

--- Module4Challenge/Pages/DadJokes.cshtml.cs
+++ Module4Challenge/Pages/DadJokes.cshtml.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Module4Challenge.Pages;
 public class IndexModel : PageModel 
-{
+{   
+    [BindProperty]
     // create attributes to describe class and store data
     public string[] DadJokes { get; set; } =
     [
